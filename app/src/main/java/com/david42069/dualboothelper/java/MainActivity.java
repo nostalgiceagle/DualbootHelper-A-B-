833d70bc36a41d699c087c6a2f48bfd1b7e9f073
Bobert (ArchUSB) <bobert@github.com>
2024-11-07 21:29:51 +0700

    Add wonderful libs from OneUI Sample app.

---
+++ app/src/main/java/com/david42069/dualboothelper/java/MainActivity.java
@@ -0,0 +1,76 @@
+package com.david42069.dualboothelper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import dev.oneuiproject.oneui.layout.ToolbarLayout;
+import dev.oneuiproject.oneui.utils.ActivityUtils;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import android.view.View;
+import android.content.Context;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import android.graphics.Color;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.CountDownTimer;
+import android.net.Uri;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ToolbarLayout toolbarLayout = findViewById(R.id.toolbar_layout);
+        toolbarLayout.setNavigationButtonAsBack();
+
+        /*TODO:
+         *  - change packageName and name
+         *  - check other TODOs
+         * */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_app_info) {
+            ActivityUtils.startPopOverActivity(this,
+                    new Intent(this, AboutActivity.class),
+                    null,
+                    ActivityUtils.POP_OVER_POSITION_RIGHT | ActivityUtils.POP_OVER_POSITION_TOP);
+            return true;
+        }
+        return false;
+    }
+}

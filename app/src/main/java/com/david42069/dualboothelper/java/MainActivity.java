66538e185b6d5c0f49ecd047e2c2f7178a06019d
justin_hson <51263008+justin-a30@users.noreply.github.com>
2024-11-09 22:43:22 +0700

    OK

--- app/src/main/java/com/david42069/dualboothelper/java/MainActivity.java
+++ app/src/main/java/com/david42069/dualboothelper/java/MainActivity.java
@@ -146,9 +146,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void executeShellCommand(String scriptFile) {
-        Shell.cmd(getResources().openRawResource(scriptFile)).exec();
+        // Extract the resource name (e.g., "switchbr" from "R.raw.switchbr")
+        String resourceName = scriptFile.replace("R.raw.", "");
+        
+        // Use getResources().getIdentifier to get the resource ID
+        int resourceId = getResources().getIdentifier(resourceName, "raw", getPackageName());
+        
+        // Now pass the resource ID into openRawResource
+        Shell.cmd(getResources().openRawResource(resourceId)).exec();
     }
-
     @Override
     public boolean onCreateOptionsMenu(@NonNull Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);

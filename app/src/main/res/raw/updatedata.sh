6269a70495d4fd2f8196ed3d1eee8d08aad39218
Bobert (ArchUSB) <bobert@github.com>
2024-11-14 13:10:49 +0700

    Do NOT modify TXT on /cache, only copy it. How this work? - If user modify data on cache -- After app initialization and updatedata.sh initialization, the slot*.txt will be modified on APP DATA side. After that, it will copy the slot file name from cache to the app data path.

--- app/src/main/res/raw/updatedata.sh
+++ app/src/main/res/raw/updatedata.sh
@@ -30,7 +30,6 @@ JQ_PATH="$DATA_PATH/files/jq"
 chmod 755 "$DATA_PATH/files/*"
 none=0
 mkdir -p /cache/dualboot/database/
-cp -a /cache/dualboot/database/slot*.txt "$DATA_PATH/files"
 #use path according if device uses EMMC/EMMC5/UFS
 use_caps=0 
 failsafe=0 
@@ -110,9 +109,7 @@ BUILD_NUMBER=$(getprop ro.build.display.id)
 if [[ "$userdata_b_num" -eq 0 ]] 
 then
 echo -e "##NOT_INSTALLED##" > "$DATA_PATH/files/status.txt"
-echo "$BUILD_NUMBER" > /cache/dualboot/database/slota.txt
 echo "$BUILD_NUMBER" > "$DATA_PATH/files/slota.txt"
-echo "##UNAVAILABLE##" > /cache/dualboot/database/slotb.txt
 echo "##UNAVAILABLE##" > $DATA_PATH/files/slotb.txt
 else
 if [[ "$efs_b_num" -ne 0 ]] 
@@ -154,9 +151,9 @@ fi
 
 if [[ "$userdata_a_num" -lt "$userdata_b_num" ]]
 then
-echo "$BUILD_NUMBER" > /cache/dualboot/database/slota.txt
 echo "$BUILD_NUMBER" > "$DATA_PATH/files/slota.txt"
 else
-echo "$BUILD_NUMBER" > /cache/dualboot/database/slotb.txt
 echo "$BUILD_NUMBER" > "$DATA_PATH/files/slotb.txt"
 fi
+
+cp -a /cache/dualboot/database/slot*.txt "$DATA_PATH/files"

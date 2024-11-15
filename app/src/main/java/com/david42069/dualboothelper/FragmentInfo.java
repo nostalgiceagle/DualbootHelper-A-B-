5f8202e38fcbd5659082de81f6b35797b4bdffd9
Bobert (ArchUSB) <bobert@github.com>
2024-11-15 20:52:54 +0700

    Let's play! Added preferences stuff String modifications Add stuffs to build.gradle

---
+++ app/src/main/java/com/david42069/dualboothelper/FragmentInfo.java
@@ -0,0 +1,12 @@
+package com.david42069.dualboothelper;
+// from OneUI Sample app. Credits to everyone who contributed in making the app.
+
+public interface FragmentInfo {
+    int getLayoutResId();
+
+    int getIconResId();
+
+    CharSequence getTitle();
+
+    boolean isAppBarEnabled();
+}
